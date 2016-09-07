cdc44ed79aa821355436bee551aecdaf3a1c0485
boris-felix <boris.felix@img.com>
2016-09-07 15:23:12 +0100

    Adding redux devtools

--- src/js/store.js
+++ src/js/store.js
@@ -1,6 +1,8 @@
 import { compose, createStore } from 'redux';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = compose()(createStore);
+const createStoreWithMiddleware = compose(
+	window.devToolsExtension ? window.devToolsExtension() : f => f
+)(createStore);
 
 export default createStoreWithMiddleware(reducers);
